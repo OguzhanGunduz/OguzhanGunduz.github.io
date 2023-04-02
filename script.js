beff7d6f36b8fd7e31ba8301143ac18238e7533f
Oguzhan Gnd <cakirdilgoz1@gmail.com>
2023-04-02 14:42:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 function calculate() {
-    var party_a = parseFloat(document.getElementById("party1").value);
-    var party_b = parseFloat(document.getElementById("party2").value);
-    var party_c = parseFloat(document.getElementById("party3").value);
-    var party_d = parseFloat(document.getElementById("party4").value);
-    var party_e = parseFloat(document.getElementById("party5").value);
+  const totalSeats = document.getElementById("totalSeats").value;
+  const party1 = document.getElementById("party1").value;
+  const party2 = document.getElementById("party2").value;
+  const party3 = document.getElementById("party3").value;
+  const party4 = document.getElementById("party4").value;
+  const party5 = document.getElementById("party5").value;
 
-    
-    var party_votes = {
-        "Party A": party_a,
-        "Party B": party_b,
-        "Party C": party_c,
-        "Party D": party_d,
-        "Party E": party_e
-    };
+  const parties = [
+    { name: "Parti 1", votes: party1, seats: 0 },
+    { name: "Parti 2", votes: party2, seats: 0 },
+    { name: "Parti 3", votes: party3, seats: 0 },
+    { name: "Parti 4", votes: party4, seats: 0 },
+    { name: "Parti 5", votes: party5, seats: 0 }
+  ];
 
-    var num_seats = 28; // bu değeri değiştirebilirsiniz
+  for (let i = 0; i < totalSeats; i++) {
+    let max = 0;
+    let index = 0;
 
-    var results = "<h2>Seat Allocation</h2><ul>";
-    var seats = dhondt_system(num_seats, party_votes);
-    for (var party in seats) {
-        results += "<li>" + party + ": " + seats[party] + " seat(s)</li>";
+    for (let j = 0; j < parties.length; j++) {
+      let quotient = parties[j].votes / (parties[j].seats + 1);
+      if (quotient > max) {
+        max = quotient;
+        index = j;
+      }
     }
-    results += "</ul>";
 
-    document.getElementById("result").innerHTML = results;
+    parties[index].seats++;
+  }
+
+  let result = "";
+
+  for (let i = 0; i < parties.length; i++) {
+    result += parties[i].name + " - " + parties[i].seats + " vekil<br>";
+  }
+
+  document.getElementById("result").innerHTML = result;
 }
